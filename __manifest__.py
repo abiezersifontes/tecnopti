01b655e622a1d644cdc0324766d0ccf23ee0e177
root <root@vps205190.vps.ovh.ca>
2019-07-17 15:13:59 -0400

    add html file in blog post

---
+++ __manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "tecnopti",
+
+    'summary': """
+        This module are destine for made all modification for tecnopti's odoo instance""",
+
+    'description': """
+        This module permit add html edit to blog post
+    """,
+
+    'author': "Tecnopti",
+    'website': "http://www.tecnopti.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','website_blog'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/website_blog_post.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
